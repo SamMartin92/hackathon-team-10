5dd7b0a6d4eaedcbc814f713b2668a9e59833d19
Christopher Meddings <c.m.meddings@gmail.com>
2024-02-17 16:42:14 +0000

    Updated cahce

--- travelcupid/settings.py
+++ travelcupid/settings.py
@@ -28,11 +28,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '.herokuapp.com',
+    '8000-cmed01-hackathonteam10-2nn83g7rtft.ws-eu108.gitpod.io',
 ]
 
 # Application definition
