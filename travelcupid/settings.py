d8d40b4efd5047106484c92fcf6f7854f2a5ecae
SamMartin92 <samm92@live.ie>
2024-02-18 21:04:56 +0000

    add whitenoise to settings

--- travelcupid/settings.py
+++ travelcupid/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'destinations',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
@@ -130,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
