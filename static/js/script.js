e56264be8a7f7fad1c0e83ec86a4e08181a590e5
SamMartin92 <samm92@live.ie>
2024-02-18 16:32:51 +0000

    markers for destinations showing on map

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,38 @@
-// Initializes Google maps and displays map
+
 let map;
+let coordsArray = [];
+let coordsString = document.getElementById('coords').innerText;
+
 
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
 
+    var coords = coordsString.match(/{lat: ([-\d.]+), lng: ([-\d.]+)}/g);
+
+    coords.forEach(function(coord) {
+        // Extract latitude and longitude from each coordinate string
+        var matches = coord.match(/{lat: ([-\d.]+), lng: ([-\d.]+)}/);
+        var lat = parseFloat(matches[1]);
+        var lng = parseFloat(matches[2]);
+
+        coordsArray.push({lat: lat, lng: lng});
+    });
+
+    console.log(coordsArray)
 
     map = new Map(document.getElementById("map"), {
         center: { lat: 50.07991, lng: -5.69756 },
         zoom: 8,
     });
-    new google.maps.Marker({
-        position: { lat: 50.07991, lng: -5.69756 },
-        map: map
-    });
+
+    for (let i=0; i < coordsArray.length; i++){
+        new google.maps.Marker({
+            position: coordsArray[i],
+            map: map
+        });
+    }
 
 }
+
 
 initMap();
